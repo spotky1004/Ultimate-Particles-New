ba04994f8b5c45b1f7d1b6651c21e15175f6644e
Spotky1004 <spotky1004@gmail.com>
2021-12-12 19:50:10 +0900

    chore: Remove stage load

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import loadStage from "./loadStage.js";
 
-const stageLoaded = await loadStage("https://jsonblob.com/api/jsonBlob/918512895818743808");
-stageLoaded.play();
+// const stageLoaded = await loadStage("https://jsonblob.com/api/jsonBlob/918512895818743808");
+// stageLoaded.play();
 
-let lastTick = new Date().getTime();
-function tick() {
-  const timeNow = new Date().getTime();
-  const dt = timeNow - lastTick;
-  lastTick = timeNow;
+// let lastTick = new Date().getTime();
+// function tick() {
+//   const timeNow = new Date().getTime();
+//   const dt = timeNow - lastTick;
+//   lastTick = timeNow;
 
-  stageLoaded.tick(dt);
+//   stageLoaded.tick(dt);
 
-  requestAnimationFrame(tick);
-}
-tick();
+//   requestAnimationFrame(tick);
+// }
+// tick();
