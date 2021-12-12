62352ec13ae27a22c94f90916174bbcd6c2d31f7
Spotky1004 <spotky1004@gmail.com>
2021-12-13 02:27:20 +0900

    chore: Particle use Value

--- src/Particle.js
+++ src/Particle.js
@@ -1,42 +1,44 @@
+import Value from "./Value.js";
+
 /**
  * @typedef Vector2
- * @property {number} x
- * @property {number} y
+ * @property {T} width
+ * @property {T} height
+ * @template T
  */
 /**
  * @typedef Size
- * @property {number} width
- * @property {number} height
+ * @property {T} width
+ * @property {T} height
+ * @template T
  */
 
 /**
  * @typedef ParticleOptions
  * @property {string} id - Id of the particle, use to recognize particle.
- * @property {Size} [size] - Size of the particle.
- * @property {Vector2} [position] - Position of the particle.
+ * @property {Size<number | string>} [size] - Size of the particle.
+ * @property {Vector2<number | string>} [position] - Position of the particle.
  * @property {string} [color] - Color of the particle.
  */
 /**
  * @typedef DrawData
- * @property {Vector2} position
- * @property {Size} size
+ * @property {Vector2<number>} position
+ * @property {Size<number>} size
  */
 
 class Particle {
   /**
    * @param {ParticleOptions} options 
    */
-  constructor(options) {
-    /** @type {string} */
-    this.id = options.id;
-    /** @type {Vector2} */
-    this.position = options.position ?? { x: 50, y: 50 };
-    /** @type {Size} */
-    this.size = options.size ?? { width: 2, height: 2 };
-    /** @type {string} */
-    this.color = options.color ?? "#000";
-  }
+  constructor(options, variables) {
+    this.variables = {...variables};
 
+    /** @type {string} */
+    this._id = options.id;
+    this._position = new Value(options.position ?? { x: 50, y: 50 });
+    this._size = new Value(options.size ?? { width: 2, height: 2 });
+    this._color = new Value(options.color ?? "#000");
+  }
 
 
   /**
