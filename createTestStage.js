fe38ee5a9e4da978cccebd0e80411d33485f805c
Spotky1004 <spotky1004@gmail.com>
2021-12-15 01:47:17 +0900

    test: Create testStage

---
+++ createTestStage.js
@@ -0,0 +1,28 @@
+import Stage from "./src/Stage.js";
+import Action from "./src/Action.js";
+
+let stageData = {
+  actions: []
+};
+
+for (let i = 0; i < 10; i++) {
+  /** @type {[keyof import("./src/Action.js").ActionDatas, number, import("./src/Particle").ParticleOptions]} */
+  let actionData = [
+    "CreateParticle",
+    (i**2+1) * 250,
+    {
+      id: "testParticle" + (i + 1),
+      variables: {
+        i: i
+      },
+      position: {
+        x: `$t/($i+15)`,
+        y: "($i+1)*10"
+      }
+    }
+  ];
+  stageData.actions.push(new Action(...actionData));
+}
+
+let stage = new Stage(stageData);
+console.log(stage.toString());
