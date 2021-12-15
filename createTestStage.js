0403bff157aeaf759a602c4b7af0cac7eb1b7cd2
Spotky1004 <spotky1004@gmail.com>
2021-12-15 23:50:39 +0900

    chore: Update test stage

--- createTestStage.js
+++ createTestStage.js
@@ -5,32 +5,30 @@ let stageData = {
   actions: []
 };
 
-for (let i = 0; i < 1; i++) {
-  /** @type {[keyof import("./src/Action.js").ActionDatas, number, import("./src/Particle").ParticleOptions, import("./src/Action.js").ActionLooper]} */
-  let actionData = [
-    "CreateParticle",
-    (i**2+1) * 250,
-    {
-      id: "\"testParticle\"+$i",
-      variables: {
-        i: i
-      },
-      position: {
-        x: "($t/10)/(15-$i)",
-        y: "($i+1)*10"
-      },
-      size: {
-        width: "$i/2+1",
-        height: "$i/2+1"
-      }
+/** @type {[keyof import("./src/Action.js").ActionDatas, number, import("./src/Particle").ParticleOptions, import("./src/Action.js").ActionLooper]} */
+let actionData = [
+  "CreateParticle",
+  (i**2+1) * 250,
+  {
+    id: "\"testParticle\"+$i",
+    variables: {
+      i: i
     },
-    {
-      loopCount: 10,
-      interval: "500*$i"
+    position: {
+      x: "($t/10)/(15-$i)",
+      y: "($i+1)*10"
+    },
+    size: {
+      width: "$i/2+1",
+      height: "$i/2+1"
     }
-  ];
-  stageData.actions.push(new Action(...actionData));
-}
+  },
+  {
+    loopCount: 10,
+    interval: "500*$i"
+  }
+];
+stageData.actions.push(new Action(...actionData));
 
 let stage = new Stage(stageData);
 // console.log(stage.toString());
