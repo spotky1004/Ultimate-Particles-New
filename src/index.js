743894c3d207e6916fedcbbac04c2dcdd92d274b
Spotky1004 <spotky1004@gmail.com>
2021-12-17 23:18:48 +0900

    chore: Add stage check

--- src/index.js
+++ src/index.js
@@ -10,11 +10,13 @@ window.stageLoaded = stageLoaded;
 
 let lastTick = new Date().getTime();
 function tick() {
-  const timeNow = new Date().getTime();
-  const dt = timeNow - lastTick;
-  lastTick = timeNow;
-
-  stageLoaded.tick(dt);
+  if (stageLoaded && stageLoaded instanceof Stage) {
+    const timeNow = new Date().getTime();
+    const dt = timeNow - lastTick;
+    lastTick = timeNow;
+  
+    stageLoaded.tick(dt);
+  }
 
   requestAnimationFrame(tick);
 }
