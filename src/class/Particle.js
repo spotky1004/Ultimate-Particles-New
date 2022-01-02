4f7c0efc1f58c168239ca46ae6218c5bbb0bae77
Spotky1004 <spotky1004@gmail.com>
2022-01-02 23:33:48 +0900

    Change particle variables priority

--- src/class/Particle.js
+++ src/class/Particle.js
@@ -94,8 +94,8 @@ class Particle {
 
   updateValues(variables) {
     const _variables = {};
-    for (const key in this.variables) _variables[key] = this.variables[key];
     for (const key in variables) _variables[key] = variables[key];
+    for (const key in this.variables) _variables[key] = this.variables[key];
 
     this.values = {
       group: this.group,
