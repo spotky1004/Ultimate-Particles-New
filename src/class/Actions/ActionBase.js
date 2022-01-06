e9b65b2782013e0bc70689aec4b8fde896de972c
Spotky1004 <spotky1004@gmail.com>
2022-01-07 00:47:03 +0900

    Create ActionBase class

---
+++ src/class/Actions/ActionBase.js
@@ -0,0 +1,55 @@
+import Value from "../Value.js";
+
+/**
+ * @typedef LooperData
+ * @property {number | string} interval
+ * @property {number} loopCount
+ * @property {number} innerLoop
+ */
+
+class ActionBase {
+  /**
+   * @param {object} obj
+   * @param {string} obj.type
+   * @param {number} obj.startTime 
+   * @param {Object.<string, any>} obj.data 
+   * @param {LooperData} obj.looperData 
+   */
+  constructor({ type, startTime=0, data={}, looperData={} }) {
+    /** @type {string} */
+    this.type = type;
+    /** @type {number} */
+    this.startTime = startTime;
+    /** @type {Object.<string, any>} */
+    this.data = data;
+    /** @type {number} */
+    this.rawInterval = looperData?.interval;
+    /** @type {number} */
+    this.rawloopCount = looperData?.loopCount;
+    /** @type {number} */
+    this.rawInnerLoop = looperData?.innerLoop;
+
+    const _looperData = {
+      interval: this.rawInterval ?? 0,
+      loopCount: this.rawloopCount ?? 1,
+      innerLoop: this.rawInnerLoop ?? 1
+    }
+
+    /** @type {Value<_looperData>} */
+    this._looperData = new Value(_looperData);
+  }
+
+  getLooperData(loopCount=0) {
+    return this._looperData.getValue({ i: loopCount });
+  }
+
+  export() {
+    return [this.type, this.time, this.data, {interval: this.rawInterval, loopCount: this.rawloopCount, innerLoop: this.rawInnerLoop}];
+  }
+
+  toString() {
+    return this.export().toString();
+  }
+}
+
+export default ActionBase;
