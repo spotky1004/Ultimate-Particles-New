2275aa61af12fe551154506236a228536adc73f7
Spotky1004 <spotky1004@gmail.com>
2022-01-07 22:58:08 +0900

    Pack all Actions into index.js

---
+++ src/class/Actions/index.js
@@ -0,0 +1,5 @@
+export { default as CreateParticle } from "./CreateParticle.js";
+export { default as ParticleGroupEvent } from "./ParticleGroupEvent.js";
+export { default as ChangeStageAttributes } from "./ChangeStageAttributes.js";
+export { default as SetGlobalVariable } from "./SetGlobalVariable.js";
+export { default as AddStatus } from "./AddStatus.js";
