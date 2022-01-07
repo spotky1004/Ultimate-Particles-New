a90f93135ee0253bcd98ee72cb42c2b26da32e6c
Spotky1004 <spotky1004@gmail.com>
2022-01-07 21:53:22 +0900

    Create ChangeStageAttributes (3/5)

---
+++ src/class/Actions/ChangeStageAttributes.js
@@ -0,0 +1,30 @@
+import ActionBase from "./ActionBase.js";
+
+/**
+ * @typedef {object} ActionData
+ * @property {keyof import("../Stage.js").StageAttribute} name
+ * @property {number | string} value
+ */
+
+class ChangeStageAttribute extends ActionBase {
+  /**
+   * @param {object} param0 
+   * @param {ActionData} param0.data
+   */
+  constructor({ data }) {
+    super(arguments[0]);
+
+    /** @type {ActionData} */
+    this.data = data;
+  }
+
+  /**
+   * @param {import("./ActionBase.js").PerformParams} param0 
+   */
+  perform({ stage, loop=0, innerLoop=0, timeOffset=0, globalVariables={} }) {
+    const variables = this.getVariables(arguments[0]);
+    stage.playingData.stageAttribute[this.data.name] = new Value(this.data.value).getValue(variables);
+  }
+}
+
+export default ChangeStageAttribute;
