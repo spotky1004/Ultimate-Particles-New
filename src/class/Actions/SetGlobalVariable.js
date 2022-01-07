82a98a9019cf3551e4ecff689496327d097de4e5
Spotky1004 <spotky1004@gmail.com>
2022-01-07 21:58:14 +0900

    Create SetGlobalVariable (4/5)

---
+++ src/class/Actions/SetGlobalVariable.js
@@ -0,0 +1,37 @@
+import ActionBase from "./ActionBase.js";
+import Value from "../Value.js";
+
+/**
+ * @typedef {object} ActionData
+ * @property {string} name
+ * @property {number | string} value
+ */
+
+class SetGlobalVariable extends ActionBase {
+  /**
+   * @param {object} param0 
+   * @param {ActionData} param0.data
+   */
+  constructor({ data }) {
+    super(arguments[0]);
+
+    /** @type {ActionData} */
+    this.data = data;
+  }
+
+  /**
+   * @param {import("./ActionBase.js").PerformParams} param0 
+   */
+  perform({ stage, loop=0, innerLoop=0, timeOffset=0, globalVariables={} }) {
+    const variables = this.getVariables(arguments[0]);
+    const value = new Value(this.data.value).getValue(variables);
+    stage.playingData.globalVariables.changeValue({
+      key: this.data.name,
+      value: value,
+      variables: variables
+    });
+    globalVariables[this.data.name] = value;
+  }
+}
+
+export default SetGlobalVariable;
