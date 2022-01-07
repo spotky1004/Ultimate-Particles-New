134750f5d6865a4c5bd15315df997a278928a160
Spotky1004 <spotky1004@gmail.com>
2022-01-07 23:35:28 +0900

    Pack ActionDatas

--- src/class/Actions/index.js
+++ src/class/Actions/index.js
@@ -1,5 +1,13 @@
 export { default as CreateParticle } from "./CreateParticle.js";
 export { default as ParticleGroupEvent } from "./ParticleGroupEvent.js";
-export { default as ChangeStageAttributes } from "./ChangeStageAttributes.js";
 export { default as SetGlobalVariable } from "./SetGlobalVariable.js";
 export { default as AddStatus } from "./AddStatus.js";
+
+/**
+ * @typedef ActionDatas
+ * @property {import("./CreateParticle.js").ActionData} CreateParticle
+ * @property {import("./ParticleGroupEvent.js").ActionData} ParticleGroupEvent
+ * @property {import("./ChangeStageAttribute.js").ActionData} ChangeStageAttribute
+ * @property {import("./SetGlobalVariable.js").ActionData} SetGlobalVariable
+ * @property {import("./AddStatus.js").ActionData} AddStatus
+ */
