e51cd9b30f7084e7dc4183ce2a27c1cdfdbf0a2b
Spotky1004 <spotky1004@gmail.com>
2022-01-09 22:21:59 +0900

    Add comparison function (gt, lt, gte, lte, eq) in StringExpression

--- src/class/StringExpression.js
+++ src/class/StringExpression.js
@@ -207,6 +207,12 @@ class StringExpression {
     "randsign": () => Math.floor(Math.random()*2)*2-1,
 
     "not": ([a]) => !a,
+
+    "gt": ([a, b]) => a > b,
+    "gte": ([a, b]) => a >= b,
+    "lt": ([a, b]) => a < b,
+    "lte": ([a, b]) => a <= b,
+    "eq": ([a, b]) => a === b,
   }
 
   /**
