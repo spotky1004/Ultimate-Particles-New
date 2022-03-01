037f2c41e2cfafbce2262dfed72ebf1ee3827ea2
Spotky1004 <spotky1004@gmail.com>
2022-03-01 21:34:22 +0900

    Rename function

--- src/util/objectEquals.js
+++ src/util/objectEquals.js
@@ -4,7 +4,7 @@
  * @param {object} y 
  * @returns {boolean}
  */
-export function deepEqual(x, y) {
+export function objectEquals(x, y) {
   const ok = Object.keys, tx = typeof x, ty = typeof y;
   return x && y && tx === 'object' && tx === ty ? (
     ok(x).length === ok(y).length &&
