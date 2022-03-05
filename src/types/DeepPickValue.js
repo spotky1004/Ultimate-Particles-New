5567896c827a88a558e6f812622f2eebba556bad
Spotky1004 <spotky1004@gmail.com>
2022-03-05 12:30:18 +0900

    Create DeepPickValue, IsObject types

---
+++ src/types/DeepPickValue.js
@@ -0,0 +1,5 @@
+/**
+ * @template T
+ * @template {keyof T} P
+ * @typedef {T extends readonly any[] ? DeepPickValue<T[number], P> : { [K in keyof T]-? : K extends P ? T[K] : never | T[K] extends readonly any[] ? DeepPickValue<T[K][number], P> : import("./IsObject.js").IsObject<T[K]> extends true ? DeepPickValue<T[K], P> : never }[keyof T]} DeepPickValue
+ */
---
+++ src/types/IsObject.js
@@ -0,0 +1,4 @@
+/**
+ * @template T
+ * @typedef {T extends string | number ? false : true} IsObject
+ */
