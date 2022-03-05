eb2bbc7a0a710ae4ceb40342e9fe830f3d30d7d3
Spotky1004 <spotky1004@gmail.com>
2022-03-05 15:56:39 +0900

    Better DeepPickValue

--- src/util-types/DeepPickValue.js
+++ src/util-types/DeepPickValue.js
@@ -1,7 +1,12 @@
 /**
  * @template T
  * @template {keyof T} P
- * @typedef {T extends readonly any[] ? DeepPickValue<T[number], P> : { [K in keyof T]-? : K extends P ? T[K] : never | T[K] extends readonly any[] ? DeepPickValue<T[K][number], P> : import("./IsObject.js").IsObject<T[K]> extends true ? DeepPickValue<T[K], P> : never }[keyof T]} DeepPickValue
+ * @typedef {T extends readonly any[] ? DeepPickValue<T[number], P> : { [K in keyof T]-? : K extends P ? T[K] : never | RecursiveDeepPickValue<T[K], P> }[keyof T]} DeepPickValue
+ */
+/**
+ * @template T
+ * @template P
+ * @typedef {T extends readonly any[] ? DeepPickValue<T[number], P> : import("./IsObject.js").IsObject<T> extends true ? DeepPickValue<T, P> : never} RecursiveDeepPickValue
  */
 
 export default DeepPickValue;
