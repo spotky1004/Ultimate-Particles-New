4d9e6a06bcd161f54176d0fc8643be1fddd75e69
Spotky1004 <spotky1004@gmail.com>
2022-03-06 15:59:40 +0900

    Create DeepRemapObject util-types

---
+++ src/util-types/DeepRemapObject.js
@@ -0,0 +1,22 @@
+// TODO: Better HTMLElement filter
+
+/**
+ * @template T
+ * @template {keyof T} P
+ * @template {keyof T} V
+ * @typedef {import("./UnionToIntersection.js").UnionToIntersection<DeepRemapObjectUnion<T, P, V>>} DeepRemapObject
+ */
+/**
+ * @template T
+ * @template {keyof T} P
+ * @template {keyof T} V
+ * @typedef {T extends readonly any[] ? DeepRemapObject<T[number], P, V> : { [K in keyof T]-? : K extends P ? import("./CreateObject.js").CreateObj<T[P], T[V]> : never | RecursiveDeepRemapObject<T[K], P, V> }[keyof T]} DeepRemapObjectUnion
+ */
+/**
+ * @template T
+ * @template P
+ * @template V
+ * @typedef {T extends readonly any[] ? DeepRemapObjectUnion<T[number], P, V> : T extends HTMLElement ? never : import("./IsObject.js").IsObject<T> extends true ? DeepRemapObjectUnion<T, P, V> : never } RecursiveDeepRemapObject
+ */
+
+export default DeepRemapObject;
