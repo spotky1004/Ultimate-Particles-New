c45c978d4fabf7bd166dd3ec85a48e124b00b635
Spotky1004 <spotky1004@gmail.com>
2022-03-06 16:40:05 +0900

    Create Input Component

---
+++ src/class/Util/Components/Forms/Input.js
@@ -0,0 +1,57 @@
+import ComponentBase from "../ComponentBase.js";
+import Element from "../../Element.js";
+
+/**
+ * @typedef ExtraOptions
+ * @property {string} defaultValue
+ */
+/**
+ * @typedef {Omit<import("../ComponentBase.js").ComponentBaseOptions, "defaultValue"> & ExtraOptions} Options
+ */
+
+const templateElement = new Element(/** @type {const} */ ({
+  type: "div",
+  cacheAs: "wrapper",
+  classNames: "component__input",
+  childs: [
+    {
+      type: "span",
+      cacheAs: "name",
+      classNames: "component__input__name",
+    },
+    {
+      type: "input",
+      cacheAs: "value",
+      classNames: "component__input__value"
+    }
+  ]
+}));
+
+/**
+ * @extends {ComponentBase<Options["defaultValue"]>}
+ */
+class Input extends ComponentBase {
+  /**
+   * @param {Options} options 
+   */
+  constructor(options) {
+    super(options);
+    const { element, cache } = templateElement.clone();
+    /** @type {typeof element} */
+    this.element = element;
+    /** @type {typeof cache} */
+    this.cache = cache;
+    this.init();
+
+    this.cache.value.addEventListener("change", (e) => {
+      this.value = this.cache.value.value;
+    });
+  }
+
+  render() {
+    this.cache.name.innerText = this.name;
+    this.cache.value.value = this.value;
+  }
+}
+
+export default Input;
