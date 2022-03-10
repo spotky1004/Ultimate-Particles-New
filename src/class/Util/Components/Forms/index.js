69bf27f00d4ce905c15f1d3aca3a5e763b9fecef
Spotky1004 <spotky1004@gmail.com>
2022-03-10 09:21:45 +0900

    Update Forms index.js

--- src/class/Util/Components/Forms/index.js
+++ src/class/Util/Components/Forms/index.js
@@ -1,16 +1,19 @@
 export { default as Input } from "./Input.js";
 export { default as Select } from "./Select.js";
+export { default as FormBundle } from "./FormBundle.js";
 
 /**
  * @typedef FormOptions
  * @property {import("./Input.js").Options} Input
  * @property {import("./Select.js").Options} Select 
+ * @property {import("./FormBundle.js").Options} FormBundle
  */
 
 /**
  * @typedef Forms
  * @property {import("./Input.js").default} Input
  * @property {import("./Select.js").default} Select
+ * @property {import("./FormBundle.js").default} FormBundle
  */
 
 /**
