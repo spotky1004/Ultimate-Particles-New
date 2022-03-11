cf98b9922af40e9e4b8031dffb88b1a7bcc746d9
Spotky1004 <spotky1004@gmail.com>
2022-03-11 17:50:21 +0900

    Add vaildiate import in ActivateActionGroup schema

--- src/game/schemas/actions/ActivateActionGroup.js
+++ src/game/schemas/actions/ActivateActionGroup.js
@@ -2,6 +2,7 @@ import ActionSchema from "../../../class/Editor/ActionSchema.js";
 import {
   Input
 } from "../../../class/Util/Components/Forms/index.js";
+import * as vaildiate from "../../../util/vaildiateFunctions.js";
 
 const schema = new ActionSchema("Activate Action Group", {
   name: new Input(/** @type {const} */ ({
