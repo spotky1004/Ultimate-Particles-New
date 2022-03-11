4f63ab4f9ab606f3634a9746bc2c6cbb0b5638e3
Spotky1004 <spotky1004@gmail.com>
2022-03-11 13:15:18 +0900

    Add TypedComponents in index.js

--- src/class/Util/Components/Forms/index.js
+++ src/class/Util/Components/Forms/index.js
@@ -1,12 +1,14 @@
 export { default as Input } from "./Input.js";
 export { default as Select } from "./Select.js";
 export { default as Components } from "./Components.js";
+export { default as TypedComponents } from "./TypedComponents.js";
 
 /**
  * @typedef FormOptions
  * @property {import("./Input.js").Options} Input
  * @property {import("./Select.js").Options} Select 
  * @property {import("./Components.js").Options} Components
+ * @property {import("./TypedComponents.js").Options} TypedComponents
  */
 
 /**
@@ -14,6 +16,7 @@ export { default as Components } from "./Components.js";
  * @property {import("./Input.js").default} Input
  * @property {import("./Select.js").default} Select
  * @property {import("./Components.js").default} Components
+ * @property {import("./TypedComponents.js").default} TypedComponents
  */
 
 /**
