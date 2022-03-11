1df89863a53885d3f8c37f84bbdfa84f4915a21f
Spotky1004 <spotky1004@gmail.com>
2022-03-11 14:48:34 +0900

    Better ActionSchema

--- src/class/Editor/ActionSchema.js
+++ src/class/Editor/ActionSchema.js
@@ -1,3 +1,5 @@
+import Components from "../Util/Components/Forms/Components.js";
+
 /**
  * @template [T=Object.<string, import("../Util/Components/Forms/index.js").AnyForm}]
  */
@@ -9,8 +11,19 @@ class ActionSchema {
   constructor(name, forms) {
     /** @type {string} */
     this.name = name ?? "";
-    /** @type {{ [K in keyof T]-? : T[K]["rawOptions"] }} */
-    this.formOptions = Object.fromEntries(Object.entries(forms).map(([key, form]) => [key, form?.rawOptions]));
+    /** @type {Components<{ forms: T }>} */
+    this._components = new Components({ forms });
+  }
+
+  /**
+   * Create new cloned components
+   */
+  get components() {
+    return this._components.clone();
+  }
+
+  clone() {
+    return new ActionSchema(this.name, this.components.forms);
   }
 }
 
