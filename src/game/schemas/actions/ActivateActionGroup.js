a6be9ae4ce1470e2e8f174310b286c9b9a0d5dd8
Spotky1004 <spotky1004@gmail.com>
2022-03-11 14:48:46 +0900

    Better ActivateActionGroup

--- src/game/schemas/actions/ActivateActionGroup.js
+++ src/game/schemas/actions/ActivateActionGroup.js
@@ -1,12 +1,14 @@
 import ActionSchema from "../../../class/Editor/ActionSchema.js";
-import * as Forms from "../../../class/Util/Components/Forms/index.js";
+import {
+  Input
+} from "../../../class/Util/Components/Forms/index.js";
 
 const schema = new ActionSchema("Activate Action Group", {
-  name: new Forms.Input({
-    name: "Group Name",
+  name: new Forms.Input(/** @type {const} */ ({
+    name: "group name",
     defaultValue: "",
     hint: "Name of the group to activate"
-  })
+  }))
 });
 
 export default schema;
