7c2d9cad855f6db453e8912deeaa01b894870a40
Spotky1004 <spotky1004@gmail.com>
2022-03-12 12:43:20 +0900

    Create finalize function in ActionSchema

--- src/class/Editor/ActionSchema.js
+++ src/class/Editor/ActionSchema.js
@@ -2,17 +2,28 @@ import Components from "../Util/Components/Forms/Components.js";
 
 /**
  * @template [T=Object.<string, import("../Util/Components/Forms/index.js").AnyForm}]
+ * @template [U=(value: Components<{ forms: T }>)<V> => V]
  */
 class ActionSchema {
   /**
    * @param {string} name 
+   * @param {U} finalize
    * @param {T} forms
    */ 
-  constructor(name, forms) {
+  constructor(name, finalize, forms) {
     /** @type {string} */
     this.name = name ?? "";
+    /** @type {U} */
+    this.finalize = finalize;
     /** @type {Components<{ forms: T }>} */
     this._components = new Components({ forms });
+  }
+
+  /**
+   * @type {ReturnType<U>}
+   */
+  get value() {
+    return this.finalize(this._components);
   }
 
   /**
